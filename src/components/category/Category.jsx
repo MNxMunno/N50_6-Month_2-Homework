1e3bf612ed8e10471bf54c15d8f91682e077d581
MNxMunno <bilolhuzayfa@gmmail.com>
2024-04-06 01:16:10 +0500

    hali chala ishi bor tekshirme turin iltimos

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -1,7 +1,158 @@
+/* eslint-disable react/no-unknown-property */
 // import React from 'react'
 
 function Category() {
-  return <div>Category</div>;
+  const category = [
+    {
+      id: 1,
+      title: "Phones",
+      url: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="31"
+          height="31"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#000"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <rect x="5" y="2" width="14" height="20" rx="2" ry="2"></rect>
+          <line x1="12" y1="18" x2="12.01" y2="18"></line>
+        </svg>
+      ),
+    },
+    {
+      id: 2,
+      title: "Computers",
+      url: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="31"
+          height="31"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#000"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect>
+          <line x1="8" y1="21" x2="16" y2="21"></line>
+          <line x1="12" y1="17" x2="12" y2="21"></line>
+        </svg>
+      ),
+    },
+    {
+      id: 3,
+      title: "SmartWatch",
+      url: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="31"
+          height="31"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#000"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <circle cx="12" cy="12" r="7"></circle>
+          <polyline points="12 9 12 12 13.5 13.5"></polyline>
+          <path d="M16.51 17.35l-.35 3.83a2 2 0 0 1-2 1.82H9.83a2 2 0 0 1-2-1.82l-.35-3.83m.01-10.7l.35-3.83A2 2 0 0 1 9.83 1h4.35a2 2 0 0 1 2 1.82l.35 3.83"></path>
+        </svg>
+      ),
+    },
+    {
+      id: 4,
+      title: "Camera",
+      url: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="31"
+          height="31"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#000"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <g transform="translate(2 3)">
+            <path d="M20 16a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2h3l2-3h6l2 3h3a2 2 0 0 1 2 2v11z" />
+            <circle cx="10" cy="10" r="4" />
+          </g>
+        </svg>
+      ),
+    },
+    {
+      id: 5,
+      title: "HeadPhones",
+      url: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="31"
+          height="31"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#000"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path d="M3 18v-6a9 9 0 0 1 18 0v6"></path>
+          <path d="M21 19a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2h3zM3 19a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2v-3a2 2 0 0 0-2-2H3z"></path>
+        </svg>
+      ),
+    },
+    {
+      id: 6,
+      title: "Gaming",
+      url: (
+        <svg
+          width="31px"
+          height="31px"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          stroke="#000"
+        >
+          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            <path
+              d="M6 12H10M8 10V14M16 13H16.01M18 11H18.01M5.2 18H18.8C19.9201 18 20.4802 18 20.908 17.782C21.2843 17.5903 21.5903 17.2843 21.782 16.908C22 16.4802 22 15.9201 22 14.8V9.2C22 8.0799 22 7.51984 21.782 7.09202C21.5903 6.71569 21.2843 6.40973 20.908 6.21799C20.4802 6 19.9201 6 18.8 6H5.2C4.0799 6 3.51984 6 3.09202 6.21799C2.71569 6.40973 2.40973 6.71569 2.21799 7.09202C2 7.51984 2 8.07989 2 9.2V14.8C2 15.9201 2 16.4802 2.21799 16.908C2.40973 17.2843 2.71569 17.5903 3.09202 17.782C3.51984 18 4.07989 18 5.2 18Z"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
+          </g>
+        </svg>
+      ),
+    },
+  ];
+  let categoryCard = category?.map((el) => (
+    <div className="category__card" key={el.id}>
+      <div className="svg__img">{el.url}</div>
+      <p className="card__title">{el.title}</p>
+    </div>
+  ));
+  return (
+    <section className="category-section">
+      <div className="container">
+        <div className="category__content">
+          <h1 className="category__title">Browse By Category</h1>
+          <div className="category__cards">{categoryCard}</div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Category;
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,13 +1,23 @@
 // import React from "react";
 import heroImg from "../../assets/images/hero__img.png";
+import heroLogoImg from "../../assets/images/hero__logo.png";
+import { FaArrowRight } from "react-icons/fa6";
 
 function Hero() {
   return (
     <section className="hero-section">
-      <div className="contaienr">
+      <div className="container">
         <div className="hero__cards">
           <div className="hero__card">
-            <img src={heroImg} alt="" />
+            <img src={heroLogoImg} alt="img" />
+            <h1 className="hero__card-title">Up to 10% off Voucher</h1>
+            <a className="hero__card-link" href="#">
+              Shop Now
+              <FaArrowRight />
+            </a>
+          </div>
+          <div className="hero__card">
+            <img src={heroImg} alt="img" />
           </div>
         </div>
       </div>
